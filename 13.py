57ff4663aeb8f9f5392aef248620673df515cdb0
saharshirkhani <s.sh200018@yahoo.com>
2024-05-18 01:07:18 +0330

    find feature from image

---
+++ 13.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2 as cv
+img = cv.imread('home.jpg')
+gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+sift = cv.SIFT_create()
+kp = sift.detect(gray,None)
+img=cv.drawKeypoints(gray,kp,img)
+cv.imwrite('keypoints.jpg',img)
